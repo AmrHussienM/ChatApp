80dfe912a6f5c3ec420320d3d5800d7ab631a939
AmrHussienM <amr.hussien97@outlook.com>
2019-04-25 17:08:29 +0200

    -Add Feature Display lastMessage in ChatFragment using(User Adapter and user_item).

--- app/src/main/java/com/example/chatapp/Adapter/UserAdapter.java
+++ app/src/main/java/com/example/chatapp/Adapter/UserAdapter.java
@@ -12,8 +12,16 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.example.chatapp.MessageActivity;
+import com.example.chatapp.Model.Msg;
 import com.example.chatapp.Model.User;
 import com.example.chatapp.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -21,6 +29,7 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
     private List<User> listOfUsers;
     private Context context;
     private String  isChat;
+    private String theLastMessage;
 
     public UserAdapter(List<User> listOfUsers, Context context,String isChat) {
         this.listOfUsers = listOfUsers;
@@ -43,6 +52,12 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
             viewHolder.profileImage.setImageResource(R.mipmap.ic_launcher);
         }else {
             Glide.with(context).load(user.getImageURL()).into(viewHolder.profileImage);
+        }
+
+        if (isChat.equals("online")){
+            LastMessage(user.getId(),viewHolder.lastMessage);
+        }else {
+            viewHolder.lastMessage.setVisibility(View.GONE);
         }
 
         if (isChat.equals("online")){
@@ -82,12 +97,54 @@ public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
         public ImageView profileImage;
         private ImageView img_on;
         private ImageView img_off;
+        private TextView lastMessage;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            lastMessage=itemView.findViewById(R.id.lastMessage);
             userName=itemView.findViewById(R.id.username_user);
             profileImage=itemView.findViewById(R.id.profile_image_user);
             img_on=itemView.findViewById(R.id.img_on);
             img_off=itemView.findViewById(R.id.img_off);
         }
+    }
+
+    //check the last Message
+    private void LastMessage(final String userid, final TextView lastMessage){
+        theLastMessage="default";
+
+        final FirebaseUser firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+
+        DatabaseReference reference= FirebaseDatabase.getInstance().getReference("Chats");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot :dataSnapshot.getChildren()){
+                    Msg msg=snapshot.getValue(Msg.class);
+                    if (msg.getReceiver().equals(firebaseUser.getUid()) && msg.getSender().equals(userid) ||
+                    msg.getReceiver().equals(userid) && msg.getSender().equals(firebaseUser.getUid())){
+
+                        theLastMessage=msg.getMessage();
+                    }
+                }
+                switch (theLastMessage){
+                    case "default":
+                        lastMessage.setText("No Message");
+                        break;
+
+                    default:
+                        lastMessage.setText(theLastMessage);
+                        break;
+                }
+
+                theLastMessage="default";
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 }
